ebe4f442c98279b089784c9631eb865e5f281df5
Jagruti <jagruti.depan@gmail.com>
2021-10-15 19:19:35 -0500

    answered

---
+++ gitInfo.js
@@ -0,0 +1,96 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition = "git is a software that is used to manage the files or project files and share it to the centrallized system. "
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+var gitHubDefinition = "github is a git repository where we can save our files."
+
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+`init`
+`Description: it initialize the repository`
+`Code: git init reposiptory link  `
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+`clone`
+`Description: to copy the repository from the remote repository to local machine cloning of that repository is done`
+`Code: git clone <remote URL>`
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+`status`
+`Description: it basically informs the status of the repository that whether the changes have been made or not or the repository is empty or not`
+`Code: git status`
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+`add`
+`Descriptpn : It stage the file to get added to the remote machine that is github from the local machine `
+`git add <file or directory name>`
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+"Commit"
+"Description: Commit records the files changed and that can be mentioned inside the double quot"
+"Code: git commit - am "text"
+
+
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+`push`
+`Description: It push the file from the local machine to the remote machine in the selescted repository it is suppose to go`
+`Code: git push`
